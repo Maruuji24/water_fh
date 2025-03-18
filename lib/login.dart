78a06eab4a1989db4211ae1c5addfbcf66fa9124
AMarNaUndartaker <settdragon@gmail.com>
2025-03-18 17:30:36 +0700

    ปรับ login
    เพิ่ม assets กับ logo เข้าไปแล้วแต่รูปไม่ขึ้น ช่วยแก้ตรง pubspec.yaml

--- lib/login.dart
+++ lib/login.dart
@@ -10,90 +10,158 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Color(0xFF8DD3F7), // Light blue background color
+        color: Color(0xFF8DD3F7),
         width: double.infinity,
         padding: EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Logo area
-            Container(
-              width: 150,
-              height: 150,
-              margin: EdgeInsets.only(bottom: 30),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
+        child: SingleChildScrollView(  // เพิ่มตัวนี้กัน Overflow
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Logo เปลี่ยนเป็นรูป
+              Container(
+                width: 150,
+                height: 150,
+                margin: EdgeInsets.only(bottom: 30, top: 60),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Image.asset(
+                  "assets/image/logofh.png",
+                  fit: BoxFit.cover,
+                ),
               ),
-              // This is a placeholder for your logo image
-              // You can replace it with Image.asset() or Image.network()
-              child: Center(
-                child: Text(
-                  "Logo",
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 20,
+
+              Text(
+                "Log in",
+                style: TextStyle(
+                  color: Color(0xFF0A496A),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Email",
+                    style: TextStyle(
+                      color: Color(0xFF0A496A),
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Container(
+                    width: double.infinity, // แก้ width ตรงนี้
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 15),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Password",
+                    style: TextStyle(
+                      color: Color(0xFF0A496A),
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Container(
+                    width: double.infinity, // แก้ width ตรงนี้
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 40),
+
+              Container(
+                width: double.infinity, // แก้ width ตรงนี้
+                child: ElevatedButton(
+                  onPressed: () {
+                    // กด login แล้วให้ไปไหน
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF0A496A),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  child: Text(
+                    "Log in",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
                   ),
                 ),
               ),
-            ),
-            
-            Text(
-              "Log in",
-              style: TextStyle(
-                color: Color(0xFF0A496A),
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30),
-            Container(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  // กดแล้วไปไหน
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF0A496A),
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
+              
+              SizedBox(height: 15),
+              Text("Or"),
+              SizedBox(height: 15),
+
+              // Register button
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // กด register แล้วจะให้ไปไหน
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF5799BD),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
                   ),
-                ),
-                child: Text(
-                  "Log in",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
+                  child: Text(
+                    "Register",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 10), //เว้นบรรทัดนะอันนี้
-            Container(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  // กดละไปไหนดี
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF5799BD),
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                ),
-                child: Text(
-                  "Register",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-            ),
-          ],
+
+              SizedBox(height: 50), // เผื่อระยะขอบล่าง
+            ],
+          ),
         ),
       ),
     );
