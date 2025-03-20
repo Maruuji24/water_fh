f450508037961c98b5bddf0683801a2b33dcfe63
Kaeooo <suroivia.210846@gmail.com>
2025-03-21 00:53:08 +0700

    Update Profile and log out

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:water_fh/home.dart';
+import 'package:water_fh/login.dart';
 import 'package:water_fh/reminder.dart';
 import 'package:water_fh/tips.dart';
 
@@ -8,10 +9,14 @@ class Profile extends StatefulWidget {
 
   @override
   _ProfileState createState() => _ProfileState();
-
 }
-  class _ProfileState extends State <Profile> {
-    int _selectedIndex = 0;
+
+class _ProfileState extends State<Profile> {
+  int _selectedIndex = 0;
+
+  // ตัวแปรสำหรับเก็บสถานะของสวิตช์
+  bool _isFaceIdEnabled = false;
+  bool _isNotificationEnabled = false;
 
   void _onItemTapped(int index) {
     if (index == 1) {
@@ -19,12 +24,12 @@ class Profile extends StatefulWidget {
         context,
         MaterialPageRoute(builder: (context) => Reminder()),
       );
-    } else if(index == 0){
+    } else if (index == 0) {
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => HomePage()),
       );
-    } else if(index == 0){
+    } else if (index == 2) {
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => Tips()),
@@ -40,13 +45,12 @@ class Profile extends StatefulWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, // ปิดปุ่ม back
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.lightBlue[200],
         elevation: 0,
         title: Row(
           children: [
-            Image.asset("assets/image/logofh.png",
-                height: 60), // เพิ่ม Image.asset แทน Text
+            Image.asset("assets/image/logofh.png", height: 60),
           ],
         ),
         actions: [
@@ -55,8 +59,7 @@ class Profile extends StatefulWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => Profile()), 
+                MaterialPageRoute(builder: (context) => Profile()),
               );
             },
           ),
@@ -67,12 +70,12 @@ class Profile extends StatefulWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const SizedBox(height: 20),
+            const SizedBox(height: 3),
             const Text(
               'Profile',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 10),
             CircleAvatar(
               radius: 50,
               backgroundImage: AssetImage('assets/image/doremon.jpg'),
@@ -80,48 +83,72 @@ class Profile extends StatefulWidget {
             const SizedBox(height: 10),
             const Text(
               'AKKARAMARIN KOINTA',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
             ),
-            const Text('akkaraamrin.2568@gmail.com'),
-            const SizedBox(height: 20),
+            const Text(
+              'akkaraamrin.2568@gmail.com',
+              style: TextStyle(fontSize: 8),
+            ),
+            const SizedBox(height: 5),
             Card(
               color: Colors.lightBlue,
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  _buildListTile('My Account', 'Make changes to your account',
-                      Icons.account_circle, Icons.warning, true),
-                  _buildSwitchTile('Face ID/ Touch ID',
-                      'Manage your device security', Icons.fingerprint),
-                  _buildListTile(
-                      'Two-Factor Authentication',
-                      'Further secure your account for safety',
-                      Icons.security,
-                      null,
-                      false),
-                  _buildSwitchTile(
-                    'Notification',
-                    'Manage your notification',
-                    Icons.notifications,
-                  ),
-                  _buildListTile('Language', 'Manage your language',
-                      Icons.language, null, false),
-                ],
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _buildListTile('My Account', 'Make changes to your account',
+                        Icons.account_circle, Icons.warning, true, true),
+                    _buildSwitchTile(
+                        'Face ID/ Touch ID',
+                        'Manage your device security',
+                        Icons.fingerprint,
+                        _isFaceIdEnabled, (value) {
+                      setState(() {
+                        _isFaceIdEnabled = value;
+                      });
+                    }),
+                    _buildListTile(
+                        'Two-Factor Authentication',
+                        'Further secure your account for safety',
+                        Icons.security,
+                        null,
+                        false,
+                        true),
+                    _buildSwitchTile('Notification', 'Manage your notification',
+                        Icons.notifications, _isNotificationEnabled, (value) {
+                      setState(() {
+                        _isNotificationEnabled = value;
+                      });
+                    }),
+                    _buildListTile('Language', 'Manage your language',
+                        Icons.language, null, false, true),
+                  ],
+                ),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 10),
             Card(
               color: Colors.lightBlue,
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  _buildListTile('Log out', '', Icons.logout, null, false),
-                  _buildListTile('About App', '', Icons.info, null, false),
-                  _buildListTile('Help & Support', '', Icons.help, null, false),
-                ],
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Column(
+                  children: [
+                    _buildListTile(
+                        'Log out', '', Icons.logout, null, false, true),
+                    _buildListTile(
+                        'About App', '', Icons.info, null, false, true),
+                    _buildListTile(
+                        'Help & Support', '', Icons.help, null, false, true),
+                  ],
+                ),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(
+              height: 5,
+            )
           ],
         ),
       ),
@@ -141,26 +168,76 @@ class Profile extends StatefulWidget {
   }
 
   ListTile _buildListTile(String title, String subtitle, IconData icon,
-      IconData? trailingIcon, bool showTrailingIcon) {
+      IconData? trailingIcon, bool showTrailingIcon, bool showArrow) {
     return ListTile(
-      leading: Icon(icon, color: Colors.white), // กำหนดสีของไอคอนให้เป็นขาว
-      title: Text(title, style: TextStyle(color: Colors.white)),
-      subtitle: Text(subtitle, style: TextStyle(color: Colors.white)),
-      trailing: showTrailingIcon && trailingIcon != null
-          ? Icon(trailingIcon, color: Colors.red)
-          : null,
-      onTap: () {},
+      leading: Icon(icon, color: Colors.white),
+      title: Text(title,
+          style: TextStyle(color: Colors.white, fontSize: 14, height: 1.2)),
+      subtitle: Text(subtitle,
+          style: TextStyle(color: Colors.white, fontSize: 9, height: 1.2)),
+      trailing: showArrow
+          ? Icon(Icons.arrow_forward_ios,
+              color: Colors.white) // ไอคอนลูกศรชี้ขวา
+          : (showTrailingIcon && trailingIcon != null
+              ? Icon(trailingIcon, color: Colors.red)
+              : null),
+      onTap: () {
+        if (title == 'Log out') {
+          _showLogOutDialog(context);
+        }
+      },
     );
   }
 
-  SwitchListTile _buildSwitchTile(
-      String title, String subtitle, IconData icon) {
-    return SwitchListTile(
-      value: false,
-      onChanged: (value) {},
-      title: Text(title, style: TextStyle(color: Colors.white)),
-      subtitle: Text(subtitle, style: TextStyle(color: Colors.white)),
-      secondary: Icon(icon, color: Colors.white), // กำหนดสีของไอคอนให้เป็นขาว
+  void _showLogOutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('ยืนยันการออกจากระบบ'),
+          content: Text('คุณต้องการออกจากระบบหรือไม่?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('ยกเลิก'),
+              onPressed: () {
+                Navigator.of(context).pop(); // ปิด Dialog หากผู้ใช้เลือกยกเลิก
+              },
+            ),
+            TextButton(
+            child: Text('ตกลง'),
+            onPressed: () {
+              Navigator.of(context).pop();  // ปิด Dialog
+
+              // เรียกใช้ฟังก์ชันออกจากระบบ
+              _logOut(context);
+
+              // หลังจากออกจากระบบแล้วไปที่หน้า Login
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),  // เปลี่ยน LoginPage() ให้เป็นหน้าล็อกอินของคุณ
+              );
+            },
+            ),
+          ],
+        );
+      },
     );
   }
+
+  void _logOut(BuildContext context) {
+    // ตัวอย่างการออกจากระบบ
+    print("User logged out");
   }
+
+  SwitchListTile _buildSwitchTile(String title, String subtitle, IconData icon,
+      bool value, ValueChanged<bool> onChanged) {
+    return SwitchListTile(
+      value: value,
+      onChanged: onChanged,
+      title: Text(title, style: TextStyle(color: Colors.white, fontSize: 14)),
+      subtitle:
+          Text(subtitle, style: TextStyle(color: Colors.white, fontSize: 9)),
+      secondary: Icon(icon, color: Colors.white),
+    );
+  }
+}
